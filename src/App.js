89f2329b839845fa4b0addb33b267dd0d3e9364a
ChakshuGautam <chaks.gautam@gmail.com>
2022-10-19 05:56:11 +0530

    Feat: Add ra-data-fakerest to test things out

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import './App.css';
-// Writing Custom DP to verify API working
-import dataprovider from './dataprovider';
+import fakeDataProvider from 'ra-data-fakerest';
 
-// This import doesn't work. 
-import * as DP from "../node_modules/ra-core/src/dataProvider";
-
-dataprovider.update("Teacher","");
-
-// const DataProvider = DP.useDataProvider();
-// DP.UPDATE("Teacher","");
-// DP.useUpdate("Teacher","");
-
-
-
+const dataProvider = fakeDataProvider({
+  posts: [
+    { id: 0, title: 'Hello, world!' },
+    { id: 1, title: 'FooBar' },
+  ],
+  comments: [
+    { id: 0, post_id: 0, author: 'John Doe', body: 'Sensational!' },
+    { id: 1, post_id: 0, author: 'Jane Doe', body: 'I agree' },
+  ],
+});
 
 
 function App() {
+  dataProvider.getOne("posts", { id: 0 }).then((response) => { console.log(response) });
   return (
     <div className="App">
-      <h1>Hii </h1>
+      <h1>See Console Log for Data </h1>
     </div>
   );
 }
